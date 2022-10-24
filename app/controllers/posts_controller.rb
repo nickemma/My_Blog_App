98074158d545069635187af6107bd75e4a2bb47f
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-24 09:19:05 +0100

    add the controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+
+ def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(user_id: params[@user.id])
+  end
+
+  def show
+    @posts = Post.find(params[:id])
+  end
+end
