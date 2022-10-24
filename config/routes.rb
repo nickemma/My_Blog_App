a315a89d3759a6e35ddb19738275525d75f5ad13
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-24 09:21:10 +0100

    fixed routes error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users only: [:index, :show] do
-    resources :posts only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
     end
 
     root to: 'users#index'
