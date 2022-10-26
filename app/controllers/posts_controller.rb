98ad8e5739793a4f4da240025fdbf27a24c3e94b
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-26 10:35:32 +0100

    refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(user_id: params[@user.id])
+    @posts = @user.posts
   end
 
   def show
