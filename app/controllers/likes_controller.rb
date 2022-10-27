f8e7a9ed6ba5236a7b6b57eb4855c0da37d9de83
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-27 09:17:21 +0100

    add the new post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    like = current_user.likes.new(post:)
+    like = current_user.likes.new(post_id: post.id)
     if like.save!
       flash[:success] = 'Post Liked successfully'
     else
