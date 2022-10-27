13d9876cbb084d9e4abd216536bc18e12097d826
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-27 11:35:26 +0100

    add the likes functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
       resources :comments, only: [:create, :new]
-      resources :likes, only: [:create]
+        resources :likes, only: [:create]
     end
   end
     root to: 'users#index'
