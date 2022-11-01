bbef94a56778a45bd0a46ea51ce33324629914f0
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-01 22:51:04 +0100

    add the login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-  def current_user
-    User.first
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :remember_me])
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
