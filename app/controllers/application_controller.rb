0ffd3a6fbd1c83c69088b7d526f509bd505a04ab
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-01 23:21:59 +0100

    add the redirect link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end
+  # protect_from_forgery with: :exception
+
+  # before_action :update_allowed_parameters, if: :devise_controller?
+
+  # protected
+
+  # def update_allowed_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  #   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+  # end
 end
