ce9153f4ce7a7b9ee95e53f8d13880298bd4ed6c
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-02 18:21:26 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-# Rails.application.routes.draw do
-#   devise_for :users, sign_out_via: [:get, :post]
-#   resources :users, only: [:index, :show] do
-#     resources :posts, only: [:index, :show, :create, :new] do
-#       resources :comments, only: [:create, :new]
-#         resources :likes, only: [:create]
-#     end
-#   end
-#     root to: 'users#index'
-# end
+Rails.application.routes.draw do
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :create, :new] do
+      resources :comments, only: [:create, :new]
+        resources :likes, only: [:create]
+    end
+  end
+    root to: 'users#index'
+end
