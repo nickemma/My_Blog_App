b4848c2708074d0fc235004cbdb17e1548542e5c
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-02 08:46:52 +0100

    add the posts count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,15 +8,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password password_confirmation current_password])
   end
 end
- # protect_from_forgery with: :exception
-
-  # before_action :update_allowed_parameters, if: :devise_controller?
-
-  # protected
-
-  # def update_allowed_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-  #   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
-  # end
+ 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy, foreign_key: :user_id
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
   # Compare this snippet from app\models\post.rb:
   def recent_three_posts
