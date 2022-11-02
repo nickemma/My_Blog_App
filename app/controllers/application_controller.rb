748654a75af353f133d5aff5cacd54be39a2edb9
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-02 18:23:11 +0200

    configuration in app controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,5 +9,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
-end
+  end
 end
